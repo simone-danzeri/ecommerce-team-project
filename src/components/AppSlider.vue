fd62bc12c3e99cf517c40fc75f117dd8501a87d0
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-13 18:09:13 +0200

    feat: add the style for AppSlider

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -8,11 +8,80 @@
 
 <template>
 
-    <div>ciao</div>
+    <section>
+        <div class="container">
+            <div class="title-container">
+                <h3>Scopri i prodotti Made in Italy</h3>
+                <a href="#">Scopri</a>
+            </div>
+            <div class="row-slider">
+                <div class="category-container">
+                    <img src="../assets/img/idee-regalo.jpg" alt="">
+                </div>
+                <div class="category-container">
+                    <img src="../assets/img/cucina.jpg" alt="">
+                </div>
+                <div class="category-container">
+                    <img src="../assets/img/moda.jpg" alt="">
+                </div>
+                <div class="category-container">
+                    <img src="../assets/img/arredamenti.jpg" alt="">
+                </div>
+                <div class="category-container">
+                    <img src="../assets/img/benessere.jpg" alt="">
+                </div>
+                <div class="category-container">
+                    <img src="../assets/img/artigianato.jpg" alt="">
+                </div>
+                <div class="category-container">
+                    <img src="../assets/img/caffetteria.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
 
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
+    @use '../style/partials/variables' as *;
 
+    .container {
+
+        background-color: $white;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+
+        .title-container{
+            display: flex;
+            align-items: center;
+            gap: 20px;
+
+            a {
+                color: $secondary-color;
+            }
+        }
+
+        .category-container {
+            display: flex;
+            flex-direction: column;
+
+            img {
+                width: 200px;
+            }
+        }
+
+        .row-slider {
+            display: flex;
+            gap: 20px;
+            overflow-y: scroll;
+        }
+        
+
+    }
+
+    
+    
 
 </style>
