9b3e3afb695fb26bc38a686dbacda1f8e5eaff9b
simone-danzeri <s.danzeri@icloud.com>
2024-05-13 17:07:29 +0200

    feat: created AppFooter base

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        name: 'AppFooter'
+    }
+</script>
+
+<template>
+    <div>
+        ciao test
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+</style>
