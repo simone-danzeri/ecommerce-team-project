61f64bbaa77cbaecd7c48d13f7b20d448f5cabda
simone-danzeri <s.danzeri@icloud.com>
2024-05-13 18:16:01 +0200

    feat: add footer-top

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,38 @@ export default {
         AppSlider,
         AppFooter
     },
+    data () {
+        return {
+            footerKnowLinks: [
+                'Opportunità di lavoro',
+                'Informazioni su Amazon',
+                'Sostenibilità',
+                'Amazon Science'
+            ],
+            footerEarnLinks: [
+                'Vendi su Amazon',
+                'Vendi su Amazon Business',
+                'Vendi prodotti innovativi',
+                'Costruisci e proteggi il tuo marchio',
+                'Diventa affiliato',
+                'Lavora con noi',
+                'Amazon Pay'
+            ],
+            footerPaymentsLinks: [
+                'Metodi di pagamento',
+                'Convertitore di valuta Amazon',
+                'Buoni regalo',
+                'Ricarica online'
+            ],
+            footerHelpLinks: [
+                'Visualizza o traccia un ordine',
+                'Costi e modalità di spedizione',
+                'Amazon Prime',
+                'Restituisci un ordine',
+                'Servizio Clienti'
+            ]
+        }
+    }
 }
 
 </script>
@@ -25,7 +57,7 @@ export default {
     </main>
 
     <footer>
-        <AppFooter></AppFooter>
+        <AppFooter :footerKnowLinks="footerKnowLinks" :footerEarnLinks="footerEarnLinks" :footerPaymentsLinks="footerPaymentsLinks" :footerHelpLinks="footerHelpLinks"></AppFooter>
     </footer>
 
 </template>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,72 @@
 <script>
     export default {
-        name: 'AppFooter'
+        name: 'AppFooter',
+        props: {
+            footerKnowLinks: Array,
+            footerEarnLinks: Array,
+            footerPaymentsLinks: Array,
+            footerHelpLinks: Array
+        }
     }
 </script>
 
 <template>
-
+    <div class="container">
+        <div class="ms-footer-top-container d-flex justify-content-around py-4">
+            <!-- Conoscersi meglio -->
+            <div class="ms-get-to-know-links">
+                <ul>
+                    <li class="text-white fw-bold">Conoscersi Meglio</li>
+                    <li v-for="eachLink in footerKnowLinks" class="py-2">
+                        <a class="fw-lighter" href="#">{{eachLink}}</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Guadagna con Amazon -->
+            <div class="ms-earn-links">
+                <ul>
+                    <li class="text-white fw-bold">Guadagna con Amazon</li>
+                    <li v-for="eachLink in footerEarnLinks" class="py-2">
+                        <a class="fw-lighter" href="#">{{eachLink}}</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Metodi di pagamento -->
+            <div class="ms-payments-links">
+                <ul>
+                    <li class="text-white fw-bold">Metodi di pagamento</li>
+                    <li v-for="eachLink in footerPaymentsLinks" class="py-2">
+                        <a class="fw-lighter" href="#">{{eachLink}}</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Bisogno di aiuto -->
+            <div class="ms-help-links">
+                <ul>
+                    <li class="text-white fw-bold">Hai bisogno di aiuto?</li>
+                    <li v-for="eachLink in footerHelpLinks" class="py-2">
+                        <a class="fw-lighter" href="#">{{eachLink}}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 
 <style scoped lang="scss">
 @use '../style/partials/variables' as *;
+.ms-footer-top-container{
+    background-color: $secondary-color;
+    ul{
+        list-style: none;
+        li{
+            list-style: none;
+            a{
+                text-decoration: none;
+                color: #fff;
+            }
+        }
+    }
+}
 </style>
